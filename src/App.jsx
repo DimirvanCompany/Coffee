5dee94325190ad9656abe551d99c575418a068de
Dimirvan <info.dimirvan@gmail.com>
2024-08-04 19:02:34 +0330

    add HomePage Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import './App.css'
-
+import HomePage from './pages/home/HomePage'
 function App() {
 
   return (
     <>
-      
+      <HomePage/>
     </>
   )
 }
---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,13 @@
+import Navbar from '../../components/navbar/Navbar'
+import './HomePage.css'
+
+function HomePage(){
+    return(
+        <>
+            <Navbar />
+            <h1>Home</h1>
+        </>
+    )
+}
+
+export default HomePage
