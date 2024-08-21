a4daed4e0f040077ebc7d28648706452c1899d22
amirreza <amirrezakarami008@gmail.com>
2024-08-21 15:56:06 +0330

    add routes menu

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import "bootstrap/dist/css/bootstrap.rtl.min.css";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
 import ShopPage from "./pages/shop/ShopPage";
+import CoffeeMenu from "./pages/coffeeMenu/CoffeeMenu";
 function App() {
   return (
     <>
@@ -16,6 +17,8 @@ function App() {
         <Route path="/branch" element={<h1>شعبه ها</h1>} />
         <Route path="/blog" element={<h1>وبلاگ</h1>} />
         <Route path="/about-us" element={<h1>درباره ما</h1>} />
+        <Route path="*" element={<h1>صفحه یافت نشد</h1>} />
+        <Route path="/menu" element={<CoffeeMenu />} />
       </Routes>
       <Footer />
     </>
